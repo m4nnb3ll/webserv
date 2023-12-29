26ec33921da5733f3d80241882be4512a2933afa
Mourad Bouyahyaoui <mbouyahy@c2r7p1.1337.ma>
2023-12-29 22:34:35 +0100

    i fixed the problem of incompleted request and i changed the content of _requestURI

--- include/Client.hpp
+++ include/Client.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Client.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abelayad <abelayad@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mbouyahy <mbouyahy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 12:39:23 by mbouyahy          #+#    #+#             */
-/*   Updated: 2023/12/28 11:55:40 by abelayad         ###   ########.fr       */
+/*   Updated: 2023/12/29 22:15:22 by mbouyahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ class Request;
 class   Client
 {
     public:
-        bool isSend;
         Client();
         ~Client();
         Client(const Client &other);
         Client &operator=(const Client &other);
 
-        Request    *ClientRequest;//change this into vector of requests if needed
+        Request     *ClientRequest;//change this into vector of requests if needed
+        bool        isSend;
 };
 
 void PrintMap(std::map<int, Client *> *_Map);
